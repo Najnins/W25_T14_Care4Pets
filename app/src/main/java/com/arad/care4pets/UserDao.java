bebdaf58b4022e858ecfbfc6caa94cdc3085c639
Brian Pagsolingan <brian.pagsolingan@gmail.com>
2026-04-06 16:30:22 -0400

    added user entity and user dao

---
+++ app/src/main/java/com/arad/care4pets/UserDao.java
@@ -0,0 +1,18 @@
+package com.arad.care4pets;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+@Dao
+public interface UserDao {
+
+    // fail silently if email already exists
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    long insert(User user);
+
+    // Find user by email for login
+    @Query("SELECT * FROM users WHERE email = :email LIMIT 1")
+    User getUserByEmail(String email);
+}
