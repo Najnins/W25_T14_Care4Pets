9a280d4b3e408d00a347e80f2becb1830d392b9f
Muhammad Ansari <157079603+Shaheerios@users.noreply.github.com>
2026-04-24 19:26:25 -0400

    Added comments to HealthRecordDetails

--- app/src/main/java/com/arad/care4pets/ui/activities/HealthRecordDetails.java
+++ app/src/main/java/com/arad/care4pets/ui/activities/HealthRecordDetails.java
@@ -12,11 +12,13 @@ public class HealthRecordDetails extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set layout for health record details screen
         setContentView(R.layout.activity_health_record_details);
 
-
+        // Get reference to back button
         ImageButton btnBack = findViewById(R.id.btnBack);
 
+        // Close activity when back button is pressed
         btnBack.setOnClickListener(v -> finish());
     }
 }
